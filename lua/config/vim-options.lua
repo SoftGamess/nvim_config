800c8d4fc33d613107569f7dad2754147369101c
SoftGamess <raphael.sfar@gmail.com>
2025-06-20 15:49:32 +0200

    Changed theme to miasma

--- lua/config/vim-options.lua
+++ lua/config/vim-options.lua
@@ -15,4 +15,4 @@ vim.api.nvim_create_autocmd("VimEnter", {
 
 vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>',{})
 vim.keymap.set('n', '<leader>nt', ':Neotree filesystem reveal left toggle<CR>' ,{})
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme "miasma"
